26764a8c9470aa687fd40341d396dca575365d08
szdanika <danikaszab@gmail.com>
2022-03-08 09:20:30 +0100

    .

---
+++ 06-BackTrack/BackTrack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BackTrack
+{
+    abstract internal class BackTrack
+    {
+        protected int N;
+        protected int[] M;
+        protected object[,] R;
+        public abstract void ft(int x, object y);
+        public abstract void fk(int x, object y, int x0, object y0);
+        public delegate void AllapotFigyelo(int x, object[] tomb);
+        public event AllapotFigyelo Probalkozas;
+    }
+}
